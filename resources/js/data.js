5b9982d41aeb3fca0a796d12a9c24daa9ae41846
Mukesh Kumar <mukeshk371@gmail.com>
2018-10-02 18:54:47 +0530

    Flat search by no. of bedrooms, zip code

---
+++ resources/js/data.js
@@ -0,0 +1,65 @@
+var houseList = [
+    {
+        "type" : "3BHK Flat",
+        "bedrooms" : "3",
+        "pic" : "resources/images/3bhk-1.png",
+        "price" : "₹30,00,000",
+        "zip" : "842001"
+    },
+    {
+        "type" : "3BHK Flat",
+        "bedrooms" : "3",
+        "pic" : "resources/images/3bhk-2.jpg",
+        "price" : "₹30,33,000",
+        "zip" : "844127"
+    },
+    {
+        "type" : "3BHK Flat",
+        "bedrooms" : "3",
+        "pic" : "resources/images/3bhk-1.png",
+        "price" : "₹35,40,000",
+        "zip" : "842001"
+    },
+    {
+        "type" : "3BHK Flat",
+        "bedrooms" : "3",
+        "pic" : "resources/images/3bhk-2.jpg",
+        "price" : "₹40,00,000",
+        "zip" : "844127"
+    },
+    {
+        "type" : "3BHK Flat",
+        "bedrooms" : "3",
+        "pic" : "resources/images/3bhk-1.png",
+        "price" : "₹32,50,000",
+        "zip" : "842001"
+    },
+    {
+        "type" : "2BHK Flat",
+        "bedrooms" : "2",
+        "pic" : "resources/images/3bhk-1.png",
+        "price" : "₹25,00,000",
+        "zip" : "842001"
+    },
+    {
+        "type" : "2BHK Flat",
+        "bedrooms" : "2",
+        "pic" : "resources/images/3bhk-2.jpg",
+        "price" : "₹20,00,000",
+        "zip" : "844127"
+    },
+    {
+        "type" : "2BHK Flat",
+        "bedrooms" : "2",
+        "pic" : "resources/images/3bhk-1.png",
+        "price" : "₹26,55,000",
+        "zip" : "842001"
+    },
+    {
+        "type" : "2BHK Flat",
+        "bedrooms" : "2",
+        "pic" : "resources/images/3bhk-2.jpg",
+        "price" : "₹28,00,000",
+        "zip" : "844127"
+    }
+]
---
+++ resources/js/javascript_practice.js
@@ -0,0 +1,80 @@
+/* function validateForm() {
+    var income = document.orderForm.inputIncome.value;
+    var bedrooms = document.orderForm.inputBedrooms.value;
+    var zip = document.orderForm.inputZip.value;
+    
+    if (income == "" || isNaN(income)) {
+        alert("Please Enter your Income.");
+        return false;
+    } else if (bedrooms == "" || isNaN(bedrooms)) {
+        alert("Please Enter the number of bedrooms.");
+        return false;
+    } else if (zip == "" || isNaN(zip)) {
+        alert("Please Enter the ZIP Code.");
+        return false;
+    }
+} */
+
+function calculatePrice() {
+    var maxPrice = document.getElementById("income").value * 4;
+    alert("Maximum Price = ₹" + maxPrice + ".");
+}
+
+
+function createLi(information) {
+    var selectButton = document.createElement("BUTTON");
+    selectButton.classList.add("selectButton");
+    selectButton.innerHTML = "Select Flat";
+
+    var buttonLink = document.createElement("A");
+    buttonLink.setAttribute("href", "#");
+    buttonLink.appendChild(selectButton);
+
+    var flatPrice = document.createElement("H3");
+    flatPrice.innerHTML = information.price;
+
+    var flatImage = document.createElement("IMG");
+    flatImage.setAttribute("src", information.pic);
+
+    var figure = document.createElement("FIGURE");
+    figure.classList.add("house-photo");
+    figure.appendChild(flatImage);
+
+    var heading = document.createElement("H1");
+    heading.innerHTML = information.type;
+
+    var list = document.createElement("LI");
+    list.appendChild(heading);
+    list.appendChild(figure);
+    list.appendChild(flatPrice);
+    list.appendChild(buttonLink);
+
+    return list;
+}
+
+function filterHouseList() {
+    var requiredArray = [];
+    for(var i = 0; i < houseList.length; i++) {
+        var inputBedrooms = document.getElementById("bedrooms").value;
+        var inputZip = document.getElementById("zip").value;
+        if (houseList[i].bedrooms == inputBedrooms) {
+            requiredArray.push(houseList[i]);
+        }
+        else if (houseList[i].zip == inputZip) {
+            requiredArray.push(houseList[i]);
+        }
+    }
+    return requiredArray;
+}
+
+function showRequiredArray(type) {
+    var flatList = filterHouseList(type);
+    var flatShowcase = document.getElementById("house-shocase");
+    while (flatShowcase.firstChild) {
+        flatShowcase.removeChild(flatShowcase.firstChild);
+    }
+    for (var i = 0; i < flatList.length; i++) {
+        var li = createLi(flatList[i]);
+        flatShowcase.appendChild(li);
+    } 
+}
